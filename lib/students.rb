74d40cc1a75350fae7c62b32a5f4c7a70d58a19e
Jeanette Rosario <jeanetterosario88@gmail.com>
2020-05-26 00:10:21 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -1,4 +1,4 @@
-## Code your solution below. Note that your SQL queries should be in quotation marks. 
+## Code your solution below. Note that your SQL queries should be in quotation marks.
 
 def highest_student_gpa
   SELECT * FROM students ORDER BY gpa DESC LIMIT 1
@@ -9,11 +9,11 @@ def lowest_student_gpa
 end
 
 def average_student_gpa
-  SELECT AVG(gpa) FROM students; 
+  SELECT AVG(gpa) FROM students;
 end
 
 def total_tardies_for_all_students
-  
+
 end
 
 def average_gpa_for_9th_grade
